b4407f5fbd0e90b6ad48490e7609c2c1e7ca37c0
antoniostassi <stassiant@outlook.it>
2024-09-12 17:37:42 +0200

    Optimising AS_Loading usage with toRaw func

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script>
     import AS_Header from './components/AS_Header.vue';
     import AS_Main from './components/AS_Main.vue';
+    import AS_Loading from './components/AS_Loading.vue';
     import axios from 'axios';
     import { store } from './store.js';
+    import { toRaw } from 'vue'; // Per controllare se il proxy store è vuoto o meno
 
     export default {
         data(){
@@ -12,24 +14,29 @@
         },
         components:{
             AS_Header,
-            AS_Main
+            AS_Main,
+            AS_Loading
         },
         created() {
             axios
             .get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
             .then((res) => {
                 this.store.allDataCards = res.data.data;
-                console.log(res.data.data);
+
             });
         },
         methods:{
+            isStoreEmpty(storeData){
+                return toRaw(storeData)
+            }
         }
     }
 </script>
 
 <template>
     <AS_Header/>
-    <AS_Main/>
+    <AS_Loading v-show="isStoreEmpty(store.allDataCards) == []"/>
+    <AS_Main v-if="isStoreEmpty(store.allDataCards) != []"/>
 </template>
 
 <style lang="scss">
--- src/components/AS_Main.vue
+++ src/components/AS_Main.vue
@@ -1,6 +1,6 @@
 <script>
     import AS_SingleCard from './AS_SingleCard.vue';
-    import AS_Loading from './AS_Loading.vue';
+
     import { store } from '../store.js';
     export default {
         data(){
@@ -9,8 +9,7 @@
             }
         },
         components:{
-            AS_SingleCard,
-            AS_Loading
+            AS_SingleCard
         }
     }
 </script>
@@ -21,7 +20,6 @@
             <div class="bg-black py-2 text-white px-4">
                 <span v-show="store.allDataCards != []">Trovate {{ store.allDataCards.length }} carte</span>
             </div>
-            <AS_Loading v-if="store.allDataCards == []"/>
             <div v-show="store.allDataCards != []" class="py-2 px-4">
                 <div class="my-row d-flex flex-wrap">
                     <AS_SingleCard v-for="(v, i) in store.allDataCards" :cardName="v.name" :cardCategory="v.archetype" :cardImg="v.card_images[0].image_url"/>
