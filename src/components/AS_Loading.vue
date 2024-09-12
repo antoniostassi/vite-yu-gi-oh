b9df00d39a6bba6eafeeb93d7b8308eeefb44986
antoniostassi <stassiant@outlook.it>
2024-09-12 17:24:45 +0200

    AS_Loading.vue component

---
+++ src/components/AS_Loading.vue
@@ -0,0 +1,18 @@
+<script>
+
+    export default {
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1 class="text-black text-center pt-3">LOADING...</h1>
+</template>
+
+<style scoped>
+    
+</style>
--- src/components/AS_Main.vue
+++ src/components/AS_Main.vue
@@ -1,5 +1,6 @@
 <script>
     import AS_SingleCard from './AS_SingleCard.vue';
+    import AS_Loading from './AS_Loading.vue';
     import { store } from '../store.js';
     export default {
         data(){
@@ -8,7 +9,8 @@
             }
         },
         components:{
-            AS_SingleCard
+            AS_SingleCard,
+            AS_Loading
         }
     }
 </script>
@@ -17,10 +19,10 @@
     <main class="bg-warning py-5">
         <div class="container-xxl bg-white p-4">
             <div class="bg-black py-2 text-white px-4">
-                <span>Trovate {{ store.allDataCards.length }} carte</span>
+                <span v-show="store.allDataCards != []">Trovate {{ store.allDataCards.length }} carte</span>
             </div>
-            
-            <div class="py-2 px-4">
+            <AS_Loading v-if="store.allDataCards == []"/>
+            <div v-show="store.allDataCards != []" class="py-2 px-4">
                 <div class="my-row d-flex flex-wrap">
                     <AS_SingleCard v-for="(v, i) in store.allDataCards" :cardName="v.name" :cardCategory="v.archetype" :cardImg="v.card_images[0].image_url"/>
                 </div>
