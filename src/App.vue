728d13313e608d93c4f331b7e729b42a680bd6dd
antoniostassi <stassiant@outlook.it>
2024-09-12 15:37:31 +0200

    Initialising Repository

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+    import AS_Header from './components/AS_Header.vue';
+    export default {
+        data(){
+            return{
+                
+            }
+        },
+        components:{
+            AS_Header
+        }
+    }
+</script>
+
+<template>
+    <AS_Header/>
+</template>
+
+<style lang="scss" scoped>
+    @import "bootstrap/scss/bootstrap";
+    
+</style>
---
+++ src/components/AS_Header.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <header>
+        <h1>Yu Gi Oh API</h1>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
